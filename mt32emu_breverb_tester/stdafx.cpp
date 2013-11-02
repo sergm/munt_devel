ff5ea87a792c263579eb83e96029b470aefe5f39
sergm <sergm@bigmir.net>
2013-11-02 07:45:01 +0200

    - BossEmu moved to a separate dir - Updated project files - README updated

--- mt32emu_breverb_tester/stdafx.cpp
+++ mt32emu_breverb_tester/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// mt32emu_areverb_tester.pch will be the pre-compiled header
+// mt32emu_breverb_tester.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
