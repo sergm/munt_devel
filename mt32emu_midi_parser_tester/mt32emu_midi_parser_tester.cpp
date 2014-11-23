17e4ed88c9cffef32dfac81450b8b3f11c500609
sergm <sergm@bigmir.net>
2014-11-23 20:13:26 +0200

    - Updated mt32emu_midi_parser_tester to use MidiStreamParser class

--- mt32emu_midi_parser_tester/mt32emu_midi_parser_tester.cpp
+++ mt32emu_midi_parser_tester/mt32emu_midi_parser_tester.cpp
@@ -23,13 +23,36 @@ int main() {
 	const ROMImage *pcmROMImage = ROMImage::makeROMImage(&pcmROMFile);
 	ok = synth.open(*controlROMImage, *pcmROMImage, AnalogOutputMode_ACCURATE);
 	if (!ok) fail("Failed to open synth");
-	MidiParser parser(synth);
+	class : public MidiStreamParser {
+	protected:
+		// User-supplied method. Invoked when a complete short MIDI message is parsed in the input MIDI stream.
+		virtual void handleShortMessage(const Bit32u message) {
+			printf("Got ShortMessage: %06x\n", message);
+		}
+
+		// User-supplied method. Invoked when a complete well-formed System Exclusive MIDI message is parsed in the input MIDI stream.
+		virtual void handleSysex(const Bit8u stream[], const Bit32u len) {
+			std::cout << "Got SysEx:\n";
+			for (unsigned int i = 0; i < len; i++) printf("%02x ", stream[i]);
+			std::cout << std::endl;
+		}
+
+		// User-supplied method. Invoked when a System Realtime MIDI message is parsed in the input MIDI stream.
+		virtual void handleSytemRealtimeMessage(const Bit8u realtime) {
+			printf("Got SytemRealtimeMessage: %02x\n", realtime);
+		}
+
+		// User-supplied method. Invoked when an error occurs during processing the input MIDI stream.
+		virtual void printDebug(const char *debugMessage) {
+			std::cout << debugMessage << std::endl;
+		}
+	} parser;
 	Bit32u len = sizeof(stream1);
-	Bit32u parsedLength = parser.playRawMidiStream(stream1, len);
-	std::cout << "Sent=" << len << ", parsed=" << parsedLength << std::endl;
+	std::cout << "Sent " << len << " bytes\n";
+	parser.parseStream(stream1, len);
 	len = sizeof(stream2);
-	parsedLength = parser.playRawMidiStream(stream2, len);
-	std::cout << "Sent=" << len << ", parsed=" << parsedLength << std::endl;
+	std::cout << "Sent " << len << " bytes\n";
+	parser.parseStream(stream2, len);
 	_getch();
 	return 0;
 }
