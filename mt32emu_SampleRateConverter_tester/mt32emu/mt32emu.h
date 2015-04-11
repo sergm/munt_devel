10cde6e5eff8720e3979e9c27789053d75a8d616
sergm <sergm@bigmir.net>
2015-04-11 14:41:56 +0300

    - InternalResampler update for mt32emu_SampleRateConverter_tester

--- mt32emu_SampleRateConverter_tester/mt32emu/mt32emu.h
+++ mt32emu_SampleRateConverter_tester/mt32emu/mt32emu.h
@@ -3,8 +3,12 @@
 
 namespace MT32Emu {
 	typedef short Bit16s;
+	typedef int Bit32s;
 	typedef unsigned int Bit32u;
-	typedef Bit16s Sample;
+	typedef float Sample;
+	typedef float SampleEx;
+	//typedef Bit16s Sample;
+	//typedef Bit32s SampleEx;
 
 	const Bit32u MAX_SAMPLES_PER_RUN = 4096;
 	const Bit32u SAMPLE_RATE = 32000;
--- mt32emu_SampleRateConverter_tester/mt32emu_SampleRateConverter_tester.cpp
+++ mt32emu_SampleRateConverter_tester/mt32emu_SampleRateConverter_tester.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-#include "LinearResampler.h"
+#include "InternalResampler.h"
 
 using namespace MT32Emu;
 
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	Synth synth;
-	LinearResampler src(&synth, sampleRate);
+	InternalResampler src(&synth, sampleRate * 1 / 1, SampleRateConverter::SRC_GOOD);
 	Sample out[2 * MAX_SAMPLES_PER_RUN];
 	LARGE_INTEGER freq, startTime, endTime;
 	QueryPerformanceFrequency(&freq);
